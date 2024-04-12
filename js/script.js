1f9e6dd0e4c121cb3b1a0726e474fb3ebf78af22
nk125 <nekes125@outlook.com>
2024-04-12 15:49:08 -0600

    Improved plenty of things

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
+let lastElemLoadedName
+
 function modifyContent(name) {
     const mdConv = new showdown.Converter();
 
+    if (lastElemLoadedName === name) {
+        return
+    }
+    else {
+        lastElemLoadedName = name
+    }
+
     let elem = document.getElementById("mainContent")
 
-    elem.textContent = "Cargando..."
+    //elem.textContent = "Cargando..."
 
     fetch(`pages/${name}.md`, {
         mode: "same-origin",
         credentials: "same-origin",
-        referrerPolicy: "same-origin"
+        referrerPolicy: "same-origin",
+	redirect: "follow"
     }).then((data) => 
         data.text()
     ).then((text) => {
@@ -16,6 +26,19 @@ function modifyContent(name) {
     }).catch((e) => {
         elem.textContent = "Error al cargar!"
     })
+}
+
+function toggleColMainContent(isShown) {
+	let mainContent = document.getElementById("mainContent")
+
+	if (isShown) {
+		mainContent.classList.remove("col-12")
+		mainContent.classList.add("col-8")
+	}
+	else {
+		mainContent.classList.add("col-12")
+		mainContent.classList.remove("col-8")
+	}
 }
 
 let lastActiveLinkTo;
@@ -27,15 +50,17 @@ document.querySelectorAll(".linkto").forEach((link) => {
 
         modifyContent(e.target.getAttribute("render"))
 
-        link.classList.add("active")
+        link.parentNode.classList.add("bg-info")
+	link.parentNode.classList.remove("bg-primary")
 
-        if (lastActiveLinkTo !== undefined) {
-            lastActiveLinkTo.classList.remove("active")
-        }
-
-        if (lastActiveLinkTo !== link) {
-            lastActiveLinkTo = link;
-        }
+        
+	    if (lastActiveLinkTo !== undefined && lastActiveLinkTo !== link) {
+                lastActiveLinkTo.parentNode.classList.remove("bg-info")
+	        lastActiveLinkTo.parentNode.classList.add("bg-primary")
+	    }
+	    
+	    lastActiveLinkTo = link;
+	
     }
 
     link.classList.add("clickable-text")
@@ -46,13 +71,20 @@ document.querySelectorAll(".cst-nav-text").forEach((elemcls) => {
     // Clase global
     // Añadir margen - 2
     // Redondear bordes
-    // Color azul
     // Texto centrado
     // Añadir padding - 2
     elemcls.classList.add("m-2")
     elemcls.classList.add("rounded")
     elemcls.classList.add("bg-secondary")
-    elemcls.classList.add("text-center")
+    elemcls.classList.add("d-flex")
+    elemcls.classList.add("justify-content-center")
+    elemcls.classList.add("p-1")
 })
 
-modifyContent("principal")
+document.querySelectorAll(".cst-nav-text a").forEach((elemLink) => {
+    let elemTgt = elemLink.parentNode.classList;
+    elemTgt.remove("bg-secondary")
+    elemTgt.add("bg-primary")
+})
+
+document.querySelector(".linkto").click()
